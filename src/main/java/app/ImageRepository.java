7b702c4d27894563b3870817d3c469de6d620251
Bartek <bartek9393@poczta.onet.pl>
2017-05-25 20:24:00 +0100

    Changed path of files to absolute

--- src/main/java/app/ImageRepository.java
+++ src/main/java/app/ImageRepository.java
@@ -2,11 +2,13 @@ package app;
 
 import javafx.scene.image.Image;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class ImageRepository {
     public static Image get(String name) throws FileNotFoundException {
-        return new Image(new FileInputStream("C:\\Users\\bartek\\Desktop\\Projekty\\Java\\BinaryClock\\res\\" + name));
+        String filePath = new File("").getAbsolutePath();
+        return new Image(new FileInputStream(filePath + "\\resources\\" + name));
     }
 }
