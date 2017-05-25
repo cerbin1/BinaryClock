0ce1a7d5f3d3f05a44c62d8a22cf03a498c0e41b
Bartek <bartek9393@poczta.onet.pl>
2017-05-25 21:05:37 +0100

    Renamed variable

--- src/main/java/app/ImageRepository.java
+++ src/main/java/app/ImageRepository.java
@@ -8,7 +8,7 @@ import java.io.FileNotFoundException;
 
 public class ImageRepository {
     public static Image get(String name) throws FileNotFoundException {
-        String filePath = new File("").getAbsolutePath();
-        return new Image(new FileInputStream(filePath + "\\resources\\" + name));
+        String absolutePath = new File("").getAbsolutePath();
+        return new Image(new FileInputStream(absolutePath + "\\resources\\" + name));
     }
 }
