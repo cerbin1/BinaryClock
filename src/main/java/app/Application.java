3f676e7181b043f15767cad062cf5dd142eea1f4
Bartek <bartek9393@poczta.onet.pl>
2017-05-25 16:32:46 +0100

    Added refreshing binary time and displaying normal time

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,13 +1,19 @@
 package app;
 
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import static javafx.application.Platform.runLater;
 import static javafx.geometry.Pos.CENTER;
 
 public class Application extends javafx.application.Application {
@@ -21,31 +27,56 @@ public class Application extends javafx.application.Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Binary Clock");
 
+        Label label = new Label();
+
         GridPane grid = new GridPane();
         grid.setAlignment(CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
-        List[] clockDigits = binaryClock.getBinaryClock();
-        for (int i = 0; i < clockDigits.length; i++) {
-            List clockDigit = clockDigits[i];
-            for (int j = 0; j < clockDigit.size(); j++) {
-                ImageView imageView = new ImageView();
-                if ((Boolean) clockDigit.get(j)) {
-                    imageView.setImage(ImageRepository.get("1.png"));
-                } else {
-                    imageView.setImage(ImageRepository.get("0.png"));
-                }
-                grid.add(imageView, i, 3 - j);
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(() -> {
+                    try {
+                        List[] clockDigits = binaryClock.getBinaryClock();
+                        for (int i = 0; i < clockDigits.length; i++) {
+                            List clockDigit = clockDigits[i];
+                            for (int j = 0; j < clockDigit.size(); j++) {
+                                ImageView imageView = new ImageView();
+                                if ((Boolean) clockDigit.get(j)) {
+                                    imageView.setImage(ImageRepository.get("1.png"));
+                                } else {
+                                    imageView.setImage(ImageRepository.get("0.png"));
+                                }
+                                grid.add(imageView, i, 3 - j);
+                            }
+                        }
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                });
             }
-        }
+        }, 0, 1000);
+        grid.add(label, 4, 4);
+        setDisplayingTimeOn(label);
         primaryStage.setScene(new Scene(grid, 400, 400));
         primaryStage.show();
+    }
+
+    private void setDisplayingTimeOn(Label label) {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runLater(() -> label.setText(Clock.getTime()));
+            }
+        }, 0, 1000);
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-
 }
--- src/main/java/app/Clock.java
+++ src/main/java/app/Clock.java
@@ -5,7 +5,7 @@ import java.util.Calendar;
 import static java.util.Calendar.*;
 
 public class Clock {
-    void displayTime() {
+    public static String getTime() {
         Calendar c = getInstance();
         long now = c.getTimeInMillis();
         c.set(HOUR_OF_DAY, 0);
@@ -17,7 +17,7 @@ public class Clock {
         long hours = secondsPassed / 3600;
         long minutes = secondsPassed % 3600 / 60;
         long seconds = secondsPassed % 60;
-        System.out.println("It is: " + hours + ":" + minutes + ":" + seconds);
+        return "It is: " + hours + ":" + minutes + ":" + seconds;
     }
 
     public static int getPassedSecondsFromMidnight() {
