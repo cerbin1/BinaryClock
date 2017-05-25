bf7b6acf9310c462a61808d7d6ab48b502acb51c
Bartek <bartek9393@poczta.onet.pl>
2017-05-25 20:08:31 +0100

    clean

--- src/main/java/app/BinaryClock.java
+++ src/main/java/app/BinaryClock.java
@@ -3,34 +3,37 @@ package app;
 import java.util.ArrayList;
 import java.util.List;
 
+import static app.Binary.*;
+import static app.Clock.*;
+
 public class BinaryClock {
 
     public List<Boolean> getFirstDigitHour(int secondPassed) {
-        return Binary.toBinary(secondPassed / 36000, 2);
+        return toBinary(secondPassed / 36000, 2);
     }
 
     public List<Boolean> getSecondDigitHour(int secondPassed) {
-        return Binary.toBinary(secondPassed / 3600 % 10, 4);
+        return toBinary(secondPassed / 3600 % 10, 4);
     }
 
     public List<Boolean> getFirstDigitMinute(int secondPassed) {
-        return Binary.toBinary(secondPassed % 3600 / 60 / 10, 3);
+        return toBinary(secondPassed % 3600 / 60 / 10, 3);
     }
 
     public List<Boolean> getSecondDigitMinute(int secondPassed) {
-        return Binary.toBinary(secondPassed % 3600 / 60 % 10, 4);
+        return toBinary(secondPassed % 3600 / 60 % 10, 4);
     }
 
     public List<Boolean> getFirstDigitSecond(int secondPassed) {
-        return Binary.toBinary(secondPassed % 60 / 10, 3);
+        return toBinary(secondPassed % 60 / 10, 3);
     }
 
     public List<Boolean> getSecondDigitSecond(int secondPassed) {
-        return Binary.toBinary(secondPassed % 60 % 10, 4);
+        return toBinary(secondPassed % 60 % 10, 4);
     }
 
     public List[] getBinaryClock() {
-        int secondsPassed = Clock.getPassedSecondsFromMidnight();
+        int secondsPassed = getPassedSecondsFromMidnight();
         List[] clock = new ArrayList[6];
         clock[0] = getFirstDigitHour(secondsPassed);
         clock[1] = getSecondDigitHour(secondsPassed);
