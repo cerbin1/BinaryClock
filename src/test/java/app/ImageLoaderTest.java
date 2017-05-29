25cf1af418a29664719f742c66ec35a825ac639e
Bartek <bartek9393@poczta.onet.pl>
2017-05-29 16:36:05 +0100

    Added test

--- src/test/java/app/ImageLoaderTest.java
+++ src/test/java/app/ImageLoaderTest.java
@@ -1,5 +1,7 @@
 package app;
 
+import javafx.scene.image.Image;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -9,6 +11,16 @@ import java.io.FileNotFoundException;
 public class ImageLoaderTest {
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void shouldGetImage() throws FileNotFoundException {
+        // when
+
+        Image test = ImageLoader.get("test.png");
+
+        // then
+        Assert.assertFalse(test.isError());
+    }
 
     @Test
     public void shouldThrowOnGettingUnexistingFile() throws FileNotFoundException {
