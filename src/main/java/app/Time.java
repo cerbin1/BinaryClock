ef6b3489f53baf52bea25793a8647c5d3b891c71
Bartek <bartek9393@poczta.onet.pl>
2017-05-29 16:17:38 +0100

    Deleted redundant code

--- src/main/java/app/Time.java
+++ src/main/java/app/Time.java
@@ -6,17 +6,10 @@ import static java.util.Calendar.*;
 
 public class Time {
     public static String getTime() {
-        Calendar c = getInstance();
-        long now = c.getTimeInMillis();
-        c.set(HOUR_OF_DAY, 0);
-        c.set(MINUTE, 0);
-        c.set(SECOND, 0);
-        c.set(MILLISECOND, 0);
-        long passed = now - c.getTimeInMillis();
-        long secondsPassed = passed / 1000;
-        long hours = secondsPassed / 3600;
-        long minutes = secondsPassed % 3600 / 60;
-        long seconds = secondsPassed % 60;
+        int secondsPassed = getPassedSecondsFromMidnight();
+        int hours = secondsPassed / 3600;
+        int minutes = secondsPassed % 3600 / 60;
+        int seconds = secondsPassed % 60;
         return hours + ":" + (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
     }
 
